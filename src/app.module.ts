10af99b25870573a84fad9654c1046bce038d54f
Truong Phu Hung <tphung.cim@gmail.com>
2022-05-21 09:11:12 +0700

    Database config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { StudentsModule } from './students/students.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi'
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [StudentsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    StudentsModule, 
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        PORT: Joi.number()
+      })
+    }),
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ src/students/student.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+class Student {
+    @PrimaryGeneratedColumn()
+    public id: number;
+
+    @Column()
+    student_code: string;
+
+    @Column() 
+    public first_name: string;
+
+    @Column()
+    public last_name: string;
+
+    @Column()
+    public email: string;
+    
+    @Column()
+    public address: string;
+    
+    @Column()
+    public date_of_birth: Date;
+    
+    @Column()
+    public sex: string;
+}
